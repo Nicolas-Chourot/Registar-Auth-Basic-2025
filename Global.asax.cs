633fd8c0e4d3644af835ba4eb260ff9a9ee98009
Nicolas Chourot <45172002+Nicolas-Chourot@users.noreply.github.com>
2025-03-24 15:01:45 -0400

    Update prise 2

--- Global.asax.cs
+++ Global.asax.cs
@@ -1,3 +1,4 @@
+using JsonDemo.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,10 +17,11 @@ namespace JsonDemo
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            List<User> users = DB.Users.ToList();
             // Make shure there are no user still online
             for (var i = 0; i < users.Count; i++)
             {
-                 DB.Users.SetOnline(users[i], false);
+                DB.Users.SetOnline(users[i], false);
             }
         }
         protected void Session_Start()
