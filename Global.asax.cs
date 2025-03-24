8fd5519fbe101852b91eecc30d12d0cfd97cb76a
Nicolas Chourot <45172002+Nicolas-Chourot@users.noreply.github.com>
2025-03-24 08:59:15 -0400

    Reset all users online status

--- Global.asax.cs
+++ Global.asax.cs
@@ -16,7 +16,11 @@ namespace JsonDemo
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
-            
+            // Make shure there are no user still online
+            for (var i = 0; i < users.Count; i++)
+            {
+                 DB.Users.SetOnline(users[i], false);
+            }
         }
         protected void Session_Start()
         {
